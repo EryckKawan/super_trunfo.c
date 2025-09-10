2e331570325cb428139ff999faec1fcadec59ee7
Vennancio Eryck Kawan Leite Vieira <eryckkawan@hotmail.com>
2025-09-10 18:35:48 -0300

    Add files via upload

---
+++ super_trunfo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    char estado1;
+    char codigo_da_carta[5];
+    char nomecidade;
+    int populaçao;
+    float area;
+    float PIB;
+    int pontosturisticos;
+
+ 
+
+return 0;
+}
+
+
